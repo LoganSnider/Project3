c20b41f5e3d6262f00bf67abf8896731c326123a
root <root@Group1-Server.hh.nku.edu>
2022-05-03 04:41:36 -0400

    Adding File

--- testingmonit.sh
+++ testingmonit.sh
@@ -5,10 +5,12 @@ echo "Turning off ICMP"
 firewall-cmd  --add-icmp-block={echo-request,echo-reply,timestamp-reply,timestamp-request}
 
 
+echo "Checking Monit"
+monit status 
 
 sleep 2m
 
 echo "Turning on ICMP"
 firewall-cmd  --remove-icmp-block={echo-request,echo-reply,timestamp-reply,timestamp-request}
 
-
+monit status
